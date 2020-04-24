785e583b09d5354786c8f40e55e08b770f8a4f1e
Francis Pelletier <francispelletier2@hotmail.com>
2020-04-24 19:18:22 -0400

    Initial commit

---
+++ site/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const path = require("path");
+
+const app = express();
+
+app.engine(
+  "handlebars",
+  expressHandlebars({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "hbs");
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.type("text/plain");
+  res.sendFile(path.join(__dirname, "views", "layouts", "main.hbs"));
+});
+
+app.get("/about", (req, res) => {
+  res.type("text/plain");
+  res.send("About Meadowlark Travel");
+});
+
+// custom 404 page
+app.use((req, res) => {
+  res.type("text/plain");
+  res.status(404);
+  res.send("404 - Not Found");
+});
+
+// custom 500 page
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.type("text/plain");
+  res.status(500);
+  res.send("500 - Server Error");
+});
+
+app.listen(port, () =>
+  console.log(
+    `Express started on http://localhost:${port}; ` +
+      `press Ctrl-C to terminate.`
+  )
+);
